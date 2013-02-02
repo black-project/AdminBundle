ffbaf84c9d183401245004d7d5014057fc3e0154
Alexandre pocky Balmes <albalmes@gmail.com>
2013-02-03 00:08:17 +0100

    first commit

---
+++ Controller/AdminSecurityController.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the Blackengine package.
+ *
+ * (c) Alexandre Balmes <albalmes@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Blackroom\Bundle\AdminBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class AdminSecurityController extends Controller
+{
+    /**
+     * @Template()
+     */
+    public function loginAction()
+    {
+        $request = $this->getRequest();
+        $session = $request->getSession();
+
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        }
+
+        return array(
+            'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+            'error'         => $error
+        );
+    }
+}
